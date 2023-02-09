efe28de5a3686847adbcf26584247260d0d5e783
WebShining <webshinings@gmail.com>
2023-02-09 22:35:17 +0200

    ou sh

--- src/scripts/context.js
+++ src/scripts/context.js
@@ -29,3 +29,18 @@ window.onload = function() {
         return false;
     }
 };
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
